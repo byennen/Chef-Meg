ab858d56a179fe6f2dc9bd91bd7aed7371342077
Lance Ennen <byennen@gmail.com>
2011-12-26 19:27:17 -0600

    adding s3

--- app/models/recipe_photo.rb
+++ app/models/recipe_photo.rb
@@ -2,4 +2,11 @@ class RecipePhoto < ActiveRecord::Base
 
   belongs_to :recipe
 
+  has_attached_file :image,
+    :styles => { :small => "103x64!", :big => "660x346!" },
+    :storage => :s3,
+    :s3_credentials => "#{Rails.root}/config/s3.yml",
+    :path => ":attachment/:id/:style/:filename",
+    :bucket => "chef-meg-production"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,30 +11,39 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111222042717) do
+ActiveRecord::Schema.define(:version => 20111227011052) do
 
-  create_table "recipes", :force => true do |t|
-    t.string   "title"
-    t.text     "ingredients"
-    t.text     "directions"
+  create_table "recipe_photos", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  create_table "recipes", :force => true do |t|
+    t.string    "title"
+    t.text      "ingredients"
+    t.text      "directions"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "name"
+    t.string    "email",                                 :default => "", :null => false
+    t.string    "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string    "reset_password_token"
+    t.timestamp "reset_password_sent_at"
+    t.timestamp "remember_created_at"
+    t.integer   "sign_in_count",                         :default => 0
+    t.timestamp "current_sign_in_at"
+    t.timestamp "last_sign_in_at"
+    t.string    "current_sign_in_ip"
+    t.string    "last_sign_in_ip"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
